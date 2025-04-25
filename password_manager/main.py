29a7fbcec68d82e7dfd283cf58dee28ccb7a70a4
zey <128144370+zeynepseyitoglu@users.noreply.github.com>
2025-04-25 14:57:17 -0500

    Complete password app

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,8 +1,35 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 
 window = Tk()
 window.title('Password manger')
 window.config(padx=20, pady=20)
+
+def generate_password():
+    password_entry.delete(0, END)
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(0, nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(0, nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(0, nr_numbers)]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    random.shuffle(password_list)
+
+    password = ''.join(password_list)
+    password_entry.insert(0, password)
+    #Copy the password to the clipboard once its generated
+    pyperclip.copy(password)
 
 def save():
 
@@ -10,11 +37,15 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open('data.txt', 'a') as data_file:
-        data_file.write(f'{website} | {email} | {password}\n')
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+    #messagebox.showinfo(title='title', message='message')
+    is_ok = messagebox.askokcancel(title=website, message=f'These are the details that you entered:\n Email: {email}\n Password: {password}\n Is it okay to save?')
 
+    if is_ok:
+            
+        with open('data.txt', 'a') as data_file:
+            data_file.write(f'{website} | {email} | {password}\n')
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 canvas = Canvas(height=200, width=200)
@@ -38,14 +69,9 @@ email_entry.insert(0, 'zey@gmail.com')
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
-generate_password_btn = Button(text='Generate Password')
+generate_password_btn = Button(text='Generate Password', command=generate_password)
 generate_password_btn.grid(row=3, column=2)
 add_btn = Button(text='Add', width=36, command=save)
 add_btn.grid(row=4, column=1, columnspan=2)
-
-
-
-
-
 
 window.mainloop()
